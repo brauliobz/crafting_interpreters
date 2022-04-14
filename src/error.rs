f3d17d0e28a73d8ae89cd6f44cf1ed8ac584f05c
Bráulio Bezerra <brauliobezerra@gmail.com>
2022-04-14 08:51:10 -0300

    fix: include IO errors in the possible errors

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,21 @@
-#[derive(Debug, PartialEq)]
+use std::fmt::Display;
+
+#[derive(Debug)]
 pub enum Error {
     UnexpectedCharacter,
     UnterminatedString,
+    IOError(std::io::Error),
 }
+
+impl Display for Error {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Error::*;
+        match self {
+            UnexpectedCharacter => write!(fmt, "Unexpected character found"),
+            UnterminatedString => write!(fmt, "Unterminated string"),
+            IOError(err) => write!(fmt, "IO error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
